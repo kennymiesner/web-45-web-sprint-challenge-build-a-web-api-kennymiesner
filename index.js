f31adfe31efc7342901d9091ecc8bebf75748026
Kenny Miesner <84544990+kennymiesner@users.noreply.github.com>
2021-09-02 17:35:44 -0500

    Add basic server scaffolding

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
-/*
-play this: https://www.youtube.com/watch?v=d-diB65scQU
+const express = require('express')
 
-Sing along:
+const server = express()
 
-here's a little code I wrote, please read the README word for word, don't worry, you got this
-in every task there may be trouble, but if you worry you make it double, don't worry, you got this
-ain't got no sense of what is REST? just concentrate on learning Express, don't worry, you got this
-your file is getting way too big, bring a Router and make it thin, don't worry, be crafty
-there is no data on that route, just write some code, you'll sort it out… don't worry, just hack it…
-I need this code, but don't know where, perhaps should make some middleware, don't worry, just hack it
+const PORT = process.env.PORT || 5000
 
-Pull your server into this file and start it!
-*/
+server.use(express.json())
+
+server.use('*', (req, res) => {
+  res.send(`<h1>web-45-web-sprint-challenge-build-a-web-api-kennymiesner is up and running</h1>`)
+})
+
+server.listen(PORT, () => {
+  console.log(`\n *** Server Running on Port ${PORT} *** \n`)
+})
