dced9301e6cb58112a96bf654be0ba2b0031c058
Kenny Miesner <84544990+kennymiesner@users.noreply.github.com>
2021-09-03 01:48:35 -0500

    Add POST request for new action

--- api/actions/actions-middlware.js
+++ api/actions/actions-middlware.js
@@ -18,11 +18,28 @@ async function validateActionId(req, res, next) {
   }
 }
 
-// function validateAction(req, res, next) {
-//   const {}
-// }
+function validateAction(req, res, next) {
+  const { description, notes, project_id, completed } = req.body
+  if (
+    !description || !description.trim() ||
+    !notes || !notes.trim() ||
+    !project_id ||
+    completed === undefined
+  ) {
+    res.status(400).json({
+      message: 'Please include all required project_id, description, notes, and completed fields' })
+  } else if (description.length > 128) {
+    res.status(40).json({
+      message: 'Description exceeds 128 character limit'
+    })
+  } else {
+    req.description = description.trim()
+    req.notes = notes.trim()
+    next()
+  }
+}
 
 module.exports = {
   validateActionId,
-  // validateAction,
+  validateAction,
 }
--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -2,6 +2,7 @@ const express = require('express')
 const Action = require('./actions-model')
 const {
   validateActionId,
+  validateAction,
 } = require('./actions-middlware')
 
 const router = express.Router()
@@ -18,12 +19,24 @@ router.get('/:id', validateActionId, (req, res) => {
   res.status(200).json(req.action)
 })
 
-// router.post('/', validateAction, (req, res, next) => {
-//   .then(newAction => {
-//     res.status(201).json(newAction)
-//   })
-//   .catch(next)
-// })
+router.post('/', validateAction, (req, res, next) => {
+  Action.insert(req.body)
+    .then(newAction => {
+      res.status(201).json(newAction)
+    })
+    .catch(next)
+})
+
+router.put('/:id', validateActionId, validateAction, (req, res, next) => {
+  Action.update(req.params.id, req.body)
+    .then(() => {
+      return Action.get(req.params.id)
+    })
+    .then(action => {
+      res.status(200).json(action)
+    })
+    .catch(next)
+})
 
 router.delete('/:id', validateActionId, (req, res, next) => {
   Action.remove(req.params.id)
@@ -31,6 +44,14 @@ router.delete('/:id', validateActionId, (req, res, next) => {
       res.status(204).json(action)
     })
     .catch(next)
+})
+
+router.use((err, req, res, next) => { // eslint-disable-line
+  res.status(err.status || 500).json({
+    customMessage: 'actions router error',
+    message: err.message,
+    stack: err.stack,
+  })
 })
 
 module.exports = router
